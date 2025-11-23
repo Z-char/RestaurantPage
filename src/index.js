0fa86a5ae00157d0b0f764a3e683807f791ba45a
Charlie Zhang <z_char@qq.com>
2025-11-22 23:11:04 -0500

    feat: add switch logic in index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import HomeLoader from './modules/home.js';
+import MenuLoader from './modules/menu.js';
+import ContactLoader from './modules/contact.js';
+
+const tabMappings = {
+    'home-btn': HomeLoader,
+    'menu-btn': MenuLoader,
+    'contact-btn': ContactLoader,
+};
+
+(() => {
+    HomeLoader();
+    setActiveButton('home-btn');
+
+    for (const [id, handler] of Object.entries(tabMappings)) {
+        const btn = document.getElementById(id);
+        btn.addEventListener('click', () => {
+            handler();
+            setActiveButton(id);
+        })
+    }
+});
+
+function setActiveButton(id) {
+    const allButtons = document.querySelectorAll('nav button');
+    allButtons.forEach(btn => classList.remove('active'));
+
+    const activeBtn = document.getElementById(id);
+    activeBtn.classList.add('active');
+}
